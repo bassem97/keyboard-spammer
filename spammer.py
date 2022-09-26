f6f4195ec93b8cd52d3c05fc679877598126c5c1
BASSEM <bassem.jadoui@esprit.tn>
2022-09-26 23:14:31 +0100

    first commit ✅

--- spammer.py
+++ spammer.py
@@ -1,5 +1,7 @@
 import pyautogui as pg
 import time
+from pynput.keyboard import Key, Controller
+
 
 for i in range(5,0,-1):
 	print(str(i)+' seconds... ')
@@ -7,7 +9,18 @@ for i in range(5,0,-1):
 
 print('here we go !!')
 
+keyboard = Controller()
+
 for i in range(100):
-	pg.write("spam text")
+	pg.write("fcuk you... you cheating")
 	time.sleep(0.5)
+	# pg.press("Ctrl")
+	# pg.press("Enter")
+	keyboard.press(Key.ctrl)
 	pg.press("Enter")
+	# keyboard.release('enter')
+	# keyboard.release(Key.ctrl)
+
+
+fcuk you... you cheating
+
